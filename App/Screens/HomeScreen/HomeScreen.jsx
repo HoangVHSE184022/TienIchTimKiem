9659fd8e25da7a7a619791fe82c26013e5c50d12
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2024-10-02 17:15:37 +0700

    a

--- App/Screens/HomeScreen/HomeScreen.jsx
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, UrlTile } from 'react-native-maps';
 import axios from 'axios';
 import styles from './HomeScreenStyles';
 import ScreenLayout from '../ScreenLayout/ScreenLayout';
@@ -15,7 +15,7 @@ const HomeScreen = ({ navigation }) => {
   const fetchMapData = async () => {
     try {
       const response = await axios.get(
-        'https://nominatim.openstreetmap.org/search?format=json&q=Hanoi'
+        'https://nominatim.openstreetmap.org/search?format=json&q=Ba Dinh District, Hanoi, Vietnam'
       );
       if (response.data && response.data.length > 0) {
         setMapData(response.data[0]);
@@ -28,26 +28,44 @@ const HomeScreen = ({ navigation }) => {
   return (
     <ScreenLayout>  
       <View style={styles.container}>
-        <Text style={styles.title}>Màn hình chính</Text>
-        {mapData && (
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: parseFloat(mapData.lat),
-              longitude: parseFloat(mapData.lon),
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
+        <Text style={styles.title}>Bản đồ quận Ba Đình</Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 21.037457,
+            longitude: 105.829991,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}
+          minZoomLevel={13}
+          maxZoomLevel={17}
+          initialZoomLevel={15}
+        >
+          {/* Lớp bản đồ từ file MBTiles */}
+          <UrlTile
+            urlTemplate="http://192.168.100.176:3000/{z}/{x}/{y}.png"
+            zIndex={1}
+            opacity={1}
+            tileSize={256}
+            maximumZ={17}
+            minimumZ={13}
+          />
+          
+          {/* Lớp bản đồ OpenStreetMap */}
+          <UrlTile
+            urlTemplate="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            zIndex={2}
+            opacity={0.7}
+          />
+
+          <Marker
+            coordinate={{
+              latitude: 21.037457,
+              longitude: 105.829991,
             }}
-          >
-            <Marker
-              coordinate={{
-                latitude: parseFloat(mapData.lat),
-                longitude: parseFloat(mapData.lon),
-              }}
-              title={mapData.display_name}
-            />
-          </MapView>
-        )}
+            title="Quận Ba Đình"
+          />
+        </MapView>
         <TouchableOpacity
           style={styles.buttonContainer}
           onPress={() => navigation.navigate('About Us')}
