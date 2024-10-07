157bcdc728784544b36691b418202e6b4d487322
Võ Huy Hoàng <145216389+HoangVHSE184022@users.noreply.github.com>
2024-10-07 09:13:49 +0700

    test cam qr

---
+++ App/Screens/Cemetery/Cemetery.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import styles from './CemeteryStyles';
+
+export default function Cemetery() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [qrData, setQrData] = useState('');
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setQrData(data);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.camera}
+        />
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>
+          {scanned ? qrData : 'Scan a QR code to see the information here'}
+        </Text>
+      </View>
+    </View>
+  );
+}
